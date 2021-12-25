b19cf23068205c82df8cd66348766fca40cf6cda
josephmlev <joseph@campus-008-189.ucdavis.edu>
2021-12-25 14:46:14 -0800

    updates method of calculating mismatch loss to get true power delivered to antenna

--- simulationValidation/sweepScan_12.24.21/analysis_12.24.21.py
+++ simulationValidation/sweepScan_12.24.21/analysis_12.24.21.py
@@ -12,11 +12,94 @@ import pandas as pd
 import scipy as sp
 from scipy.interpolate import interp1d
 
-#52cd dipole 60N 60W 57V
+
+def interpDf(dfPath, freqArr):
+    freq = pd.read_pickle(dfPath).index.values
+    power = pd.read_pickle(dfPath)['Power (dBm)'].to_numpy()
+    interpPowerArr = interp1d(freq, power)
+    return interpPowerArr(freqArr)
+
+def interpTxt(txtPath, freqArr):
+    vswrfreq, vswr= np.loadtxt(txtPath, delimiter = ',', unpack = True)
+    interpVswrArr = interp1d(vswrfreq,vswr)
+    return interpVswrArr(freqArr)
+
+
+def processPowerDf(freqArr, interpPowerArr, interpFGenCalArr, interpVswrArr):
+    dfName = pd.DataFrame()
+
+    dfName['Frequency (MHz)'] = freqArr
+    dfName['Raw Antenna Power (dBm)'] = interpPowerArr
+    dfName['VSWR'] = interpVswrArr
+    dfName['F. Gen Output (dBm)'] = interpFGenCalArr
+    dfName['Reflection Coefficient'] = (dfName['VSWR'] - 1) /(dfName['VSWR'] + 1)
+    dfName['Matching Loss'] = -10 * np.log10(1 - dfName['Reflection Coefficient']**2)
+    dfName['Power to Antenna'] = dfName['F. Gen Output (dBm)'] - dfName['Matching Loss']
+    dfName['Normilized Antenna Power (dBm)'] = dfName['Raw Antenna Power (dBm)'] - dfName['Power to Antenna']
+    return dfName
 
 freqArr = np.arange(50,300,0.1)
 
 
+dip52cm_60_60_57_PowerArr = interpDf('./52cmDip_pwr_n34dBm_60N_60W_57V.pkl', freqArr)
+dip52cm_60_60_57_fGenCalArr = interpDf('./fGenCalHiRes.pkl', freqArr)
+dip52cm_60_60_57_VswrArr = interpTxt('52cmDip_vswr_60N_60W_57V.txt', freqArr)
+
+dip52cm_60_60_57_powerDf = processPowerDf(freqArr, dip52cm_60_60_57_PowerArr, dip52cm_60_60_57_fGenCalArr, dip52cm_60_60_57_VswrArr)
+
+
+
+
+dip30cm_60_60_57_PowerArr = interpDf('./30cmDip_pwr_n20dbm_60N_60W_57V.pkl', freqArr)
+dip30cm_60_60_57_fGenCalArr = interpDf('./fGenCal_30cmDip_n20dbm_60N_60W_57V.pkl', freqArr)
+dip30cm_60_60_57_VswrArr = interpTxt('30cmDip_vswr_60N_60W_57V.txt', freqArr)
+
+dip30cm_60_60_57_powerDf = processPowerDf(freqArr, dip30cm_60_60_57_PowerArr, dip30cm_60_60_57_fGenCalArr, dip30cm_60_60_57_VswrArr)
+
+
+
+
+'''
+'Frequency (MHz)', 'Raw Antenna Power (dBm)', 'VSWR',
+'F. Gen Output (dBm)', 'Reflection Coefficient', 'Return Loss (dB)',
+'Power to Antenna', 'Normilized Antenna Power (dBm)'
+'''
+plt.close('all')
+
+
+plt.figure()
+plt.title('Normilized Antenna Output Power')
+plt.plot(freqArr, dip30cm_60_60_57_powerDf['Normilized Antenna Power (dBm)'], label = '30cm dip')
+#plt.plot(freqArr, dip52cm_60_60_57_powerDf['Normilized Antenna Power (dBm)'], label = '52cm dip')
+plt.legend()
+plt.xlabel('Freq (MHz)')
+plt.ylabel('Power (dBm)')
+
+plt.figure()
+plt.title('Raw Antenna Output Power')
+plt.plot(freqArr, dip30cm_60_60_57_powerDf['Raw Antenna Power (dBm)'], label = '30cm dip')
+plt.plot(freqArr, dip52cm_60_60_57_powerDf['Raw Antenna Power (dBm)'], label = '52cm dip')
+plt.legend()
+plt.xlabel('Freq (MHz)')
+plt.ylabel('Power (dBm)')
+
+plt.figure()
+plt.title('Reflection Coefficient')
+plt.plot(freqArr, dip30cm_60_60_57_powerDf['Reflection Coefficient'], label = '30cm dip')
+plt.plot(freqArr, dip52cm_60_60_57_powerDf['Reflection Coefficient'], label = '52cm dip')
+plt.legend()
+plt.xlabel('Freq (MHz)')
+plt.ylabel('Power (dBm)')
+
+plt.figure()
+plt.title('VSWR')
+plt.plot(freqArr, dip30cm_60_60_57_powerDf['VSWR'], label = '30cm dip')
+plt.plot(freqArr, dip52cm_60_60_57_powerDf['VSWR'], label = '52cm dip')
+plt.legend()
+plt.xlabel('Freq (MHz)')
+plt.ylabel('Power (dBm)')
+
+'''
 dip52CmFreq = pd.read_pickle('./52cmDip_pwr_n34dBm_60N_60W_57V.pkl').index.values
 dip52CmPower = pd.read_pickle('./52cmDip_pwr_n34dBm_60N_60W_57V.pkl')['Power (dBm)'].to_numpy()
 dip52CmInterp = interp1d(dip52CmFreq, dip52CmPower)
@@ -40,5 +123,37 @@ df_52cmDip_60N_60W_57V['Power to Antenna'] = df_52cmDip_60N_60W_57V['F. Gen Outp
 df_52cmDip_60N_60W_57V['Normilized Dipole Power (dBm)'] = df_52cmDip_60N_60W_57V['Dipole 52 CM Power (dBm)'] - df_52cmDip_60N_60W_57V['Power to Antenna']
 
 df_52cmDip_60N_60W_57V.plot('Frequency (MHz)', 'Normilized Dipole Power (dBm)')
-
-
+'''
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
